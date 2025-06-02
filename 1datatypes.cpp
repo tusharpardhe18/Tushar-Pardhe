bfa8946fe0f04f1c05893110b09d06e6f84d84ca
Tushar Pardhe <tusharpardhe2018@gmail.com>
2025-06-02 20:18:16 +0530

    C++

---
+++ 1datatypes.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	/*
+	int a = 123;
+	
+	cout<<a<<endl; // 123
+	
+	char ch = 'v';
+	cout<<ch<<endl; // v
+	
+	bool b = true;
+	cout<<b<<endl; // 1
+	
+	float f =1.2;
+	cout<<f<<endl; //1.2
+	
+	double d = 1.23;
+	cout<<d<<endl; // 123
+	
+	int size = sizeof(a);
+	cout<<"Size of a is : "<<size<<endl; // 8
+
+	int q = 'a';
+	cout<<q<<endl; // 97
+	
+	char ch1 = 98;
+	cout<<ch1<<endl;  // b
+	
+	char ch2 = 123456;
+	cout<<ch2<<endl;
+	
+	unsigned int a = -122;
+	cout<<a<<endl;
+	
+	float a = 2, b = 5;
+	float res = a/b;
+	cout<<res;
+	*/
+	
+	int a = 2, b = 3;
+	
+	bool a1 = (a==b);
+	cout<<a1<<endl;
+	
+	bool b1 = (a>b);
+	cout<<b1<<endl;
+	
+	bool c1 = (a<b);
+	cout<<c1<<endl;
+	
+	bool d1 = (a>=b);
+	cout<<d1<<endl;
+	
+	bool e1 = (a<=b);
+	cout<<e1<<endl;
+	
+	bool f1 = (a!=b);
+	cout<<f1<<endl;
+	
+}
